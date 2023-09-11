3f6694ce507c8c1b007fabc33386528073eb69dc
이영은 <hisenselee12@gmail.com>
2023-09-11 18:50:30 +0900

    쓰레드 UI 구현 완료

--- src/pages/MainDetail/MainDetail.js
+++ src/pages/MainDetail/MainDetail.js
@@ -15,22 +15,24 @@ const MainDetail = () => {
           </Link>
           뒤로
         </header>
-        <div className="contents">
-          <div className="info">
-            <div className="name">
-              <img src="/images/testImg.png" />
-              <span>Name</span>
-            </div>
-            <div className="time">
-              <p>00:00:00</p>
-            </div>
+        <div className="contentsCompo">
+          <div className="nameWrap">
+            <img src="/images/testImg.png" />
+            <label>
+              Name<span>00:00:00</span>
+            </label>
           </div>
-          <p>
-            일라이자 효과는 인간의 사고 과정과 감정을 AI 시스템에 잘못 돌리는
-            사람들의 경향을 말하며, 따라서 시스템이 실제보다 더 지능적이라고
-            믿는다. 이 현상은 1966년 MIT 교수 조셉 웨이젠바움이 만든 챗봇인
-            ELIZA의 이름을 따서 명명되었다.
-          </p>
+          <div className="textBox">
+            <p>
+              일라이자 효과는 인간의 사고 과정과 감정을 AI 시스템에 잘못 돌리는
+              사람들의 경향을 말하며, 따라서 시스템이 실제보다 더 지능적이라고
+              믿는다. 이 현상은 1966년 MIT 교수 조셉 웨이젠바움이 만든 챗봇인
+              ELIZA의 이름을 따서 명명되었다.
+            </p>
+          </div>
+          <div className="commentGo">
+            <p> 댓글00</p>
+          </div>
         </div>
         <div className="comment">
           <input className="upload"></input>
@@ -45,7 +47,7 @@ const MainDetail = () => {
             <div className="postRight">
               <div className="content">
                 <div className="postHead">
-                  <span>userName</span>
+                  <span>Username</span>
                   <div className="frame">
                     <span className="Cgray60">00:00:00</span>
                     <a className="Cred">삭제</a>
