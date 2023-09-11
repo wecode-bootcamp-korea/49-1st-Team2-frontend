ca3b7264daa81d1ad078b7899ab9b7934542a971
이영은 <hisenselee12@gmail.com>
2023-09-11 11:48:43 +0900

    main ui 구현 완료

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -18,13 +18,13 @@ const Main = () => {
         <div className="contentList">
           {/* 여기 부분 map으로 불러옴. */}
           <div className="content" onClick={goDetial}>
-            <div>
+            <div className="nameWrap">
               <img src="/images/testImg.png" />
               <label>
                 Name<span>00:00:00</span>
               </label>
             </div>
-            <div>
+            <div className="textBox">
               <p>
                 일라이자 효과는 인간의 사고 과정과 감정을 AI 시스템에 잘못
                 돌리는 사람들의 경향을 말하며, 따라서 시스템이 실제보다 더
@@ -33,17 +33,17 @@ const Main = () => {
               </p>
             </div>
             <div>
-              <p>댓글00</p>
+              <p className="comment"> 댓글00</p>
             </div>
           </div>
           <div className="content" onClick={goDetial}>
-            <div>
+            <div className="nameWrap">
               <img src="/images/testImg.png" />
               <label>
                 Name<span>00:00:00</span>
               </label>
             </div>
-            <div>
+            <div className="textBox">
               <p>
                 일라이자 효과는 인간의 사고 과정과 감정을 AI 시스템에 잘못
                 돌리는 사람들의 경향을 말하며, 따라서 시스템이 실제보다 더
@@ -52,17 +52,17 @@ const Main = () => {
               </p>
             </div>
             <div>
-              <p>댓글00</p>
+              <p className="comment"> 댓글00</p>
             </div>
           </div>
           <div className="content" onClick={goDetial}>
-            <div>
+            <div className="nameWrap">
               <img src="/images/testImg.png" />
               <label>
                 Name<span>00:00:00</span>
               </label>
             </div>
-            <div>
+            <div className="textBox">
               <p>
                 일라이자 효과는 인간의 사고 과정과 감정을 AI 시스템에 잘못
                 돌리는 사람들의 경향을 말하며, 따라서 시스템이 실제보다 더
@@ -71,17 +71,17 @@ const Main = () => {
               </p>
             </div>
             <div>
-              <p>댓글00</p>
+              <p className="comment"> 댓글00</p>
             </div>
           </div>
           <div className="content" onClick={goDetial}>
-            <div>
+            <div className="nameWrap">
               <img src="/images/testImg.png" />
               <label>
                 Name<span>00:00:00</span>
               </label>
             </div>
-            <div>
+            <div className="textBox">
               <p>
                 일라이자 효과는 인간의 사고 과정과 감정을 AI 시스템에 잘못
                 돌리는 사람들의 경향을 말하며, 따라서 시스템이 실제보다 더
@@ -90,32 +90,13 @@ const Main = () => {
               </p>
             </div>
             <div>
-              <p>댓글00</p>
-            </div>
-          </div>
-          <div className="content" onClick={goDetial}>
-            <div>
-              <img src="/images/testImg.png" />
-              <label>
-                Name<span>00:00:00</span>
-              </label>
-            </div>
-            <div>
-              <p>
-                일라이자 효과는 인간의 사고 과정과 감정을 AI 시스템에 잘못
-                돌리는 사람들의 경향을 말하며, 따라서 시스템이 실제보다 더
-                지능적이라고 믿는다. 이 현상은 1966년 MIT 교수 조셉 웨이젠바움이
-                만든 챗봇인 ELIZA의 이름을 따서 명명되었다.
-              </p>
-            </div>
-            <div>
-              <p>댓글00</p>
+              <p className="comment"> 댓글00</p>
             </div>
           </div>
         </div>
         <div>
           <button className="fillBtn writeBtn" onClick={goWrite}>
-            글쓰기
+            글 쓰기
           </button>
         </div>
       </div>
