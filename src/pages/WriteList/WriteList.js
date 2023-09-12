8aed8b0fa5cf0d27565691963b2a95d06396c7dc
이영은 <hisenselee12@gmail.com>
2023-09-12 12:08:40 +0900

    게시글입력 기능 구현2

--- src/pages/WriteList/WriteList.js
+++ src/pages/WriteList/WriteList.js
@@ -1,22 +1,49 @@
 import React, { useState } from "react";
 import "./WriteList.scss";
 import { Link, useNavigate } from "react-router-dom";
+import "../Login/Login";
 
-const WriteList = () => {
+const WriteList = (props) => {
   const nav = useNavigate();
   const [textSave, setTextSave] = useState("");
 
   const handleTextSave = (e) => {
     setTextSave(e.target.value);
-    console.log(textSave);
   };
 
   const goBack = () => {
     nav("/main");
   };
 
-  const save = () => {
-    alert("준비중 !");
+  // fetch("http://10.58.52.97:8000/login", {
+  //   method: "GET",
+  // })
+  //   .then()
+  //   .then();
+
+  const req = () => {
+    fetch("http://10.58.52.97:8000/login", {
+      method: "POST",
+      body: JSON.stringify({
+        text: textSave,
+      }),
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+        Authorization: localStorage.getItem("token"),
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((result) => {
+        console.log(result);
+        if (token === "Login Successful") {
+          alert("게시글이 등록이 완료되었습니다.");
+          Link("/Main");
+        } else {
+          alert("오류가 발생했습니다.");
+        }
+      });
   };
 
   return (
@@ -25,9 +52,9 @@ const WriteList = () => {
         <div className="writeWrap">
           <img src="/images/testImg.png" />
           <div className="writeContent">
-            <p>Name</p>
+            <p>nickname</p>
             <textarea
-              Placeholder="스레드를 시작하세요."
+              placeholder="스레드를 시작하세요."
               onChange={handleTextSave}
             ></textarea>
           </div>
@@ -36,7 +63,7 @@ const WriteList = () => {
           <button className="bordBtn" onClick={goBack}>
             취소
           </button>
-          <button className="fillBtn" onClick={save}>
+          <button className="fillBtn" onClick={req}>
             게시
           </button>
         </div>
