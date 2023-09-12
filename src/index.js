9e3ef013e8237c8c167d65235d7863109d11cad2
권기완 <digh7169@gmail.com>
2023-09-12 12:09:37 +0900

    ip 삭제

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import Router from "./Router";
 import "./styles/reset.scss";
 import "./styles/common.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-global.Dong = "1111";
-global.Hyun = "2222";
-global.Eun = "3333";
 
 root.render(<Router />);
