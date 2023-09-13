8bffed99aeb11d951a268fc0e78038e3add46266
이영은 <hisenselee12@gmail.com>
2023-09-13 19:09:33 +0900

    게실글 등록 기능, UI 정돈 완료

--- src/pages/WriteList/WriteList.js
+++ src/pages/WriteList/WriteList.js
@@ -23,8 +23,6 @@ const WriteList = () => {
     nav("/main");
   };
 
-  useEffect(() => {});
-
   // fetch("http://10.58.52.97:8000/login", {
   //   method: "GET",
   // })
@@ -32,40 +30,50 @@ const WriteList = () => {
   //   .then();
 
   const req = () => {
-    fetch("http://10.58.52.59:8000/threads", {
-      method: "POST",
-      body: JSON.stringify({
-        content: textSave,
-      }),
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-        Authorization: localStorage.getItem("token"),
-      },
-    })
-      .then((res) => {
-        return res.json();
+    if (textSave.length === 0) {
+      alert("게시글을 입력해주세요.");
+    } else {
+      fetch("http://10.58.52.59:8000/threads", {
+        method: "POST",
+        body: JSON.stringify({
+          content: textSave,
+        }),
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+          Authorization: localStorage.getItem("token"),
+        },
       })
-      .then((result) => {
-        if (result.message === "post created") {
-          alert("게시글이 등록이 완료되었습니다.");
-          nav("/Main");
-        } else {
-          alert("오류가 발생했습니다.");
-        }
-      });
+        .then((res) => {
+          return res.json();
+        })
+        .then((result) => {
+          if (result.message === "post created") {
+            alert("게시글이 등록이 완료되었습니다.");
+            nav("/Main");
+          } else {
+            alert("오류가 발생했습니다.");
+          }
+        });
+    }
   };
 
   return (
     <div className="writeList">
       <div className="container">
         <div className="writeWrap">
-          <img src="/images/testImg.png" />
+          <div className="imgWrap">
+            <img src="/images/testImg.png" />
+          </div>
           <div className="writeContent">
-            <p>{nickName}</p>
-            <textarea
-              placeholder="스레드를 시작하세요."
-              onChange={handleTextSave}
-            ></textarea>
+            <div className="nickNameWrap">
+              <p>{nickName}</p>
+            </div>
+            <div className="textWrap">
+              <textarea
+                placeholder="스레드를 시작하세요."
+                onChange={handleTextSave}
+              ></textarea>
+            </div>
           </div>
         </div>
         <div className="btnWrap">
